aa64244694fb811e323c6888a8f9fa0d313c6d78
veeresh-javali18 <veereshjavali038@gmail.com>
2025-04-23 14:42:16 +0530

    first commit

---
+++ Demo.java
@@ -0,0 +1,10 @@
+public class Demo{
+    public static void main(String[] args) {
+        int x=10;
+        int y=20;
+        int sum=x+y;
+        int sub=y-x;
+        System.out.println("the sum of the result is "+sum);
+        System.out.println("the substraction of the two number is"+sub);
+    }
+}
